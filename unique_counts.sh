65040dacbadd5d98160b2600c8269fff3836edb0
Nitesh <nitesharora21@hotmail.com>
2016-01-09 12:22:50 -0500

    added files

---
+++ unique_counts.sh
@@ -0,0 +1,14 @@
+data/aiden-2015.txt 
+      1	marlin
+      1	shark
+data/dmitri-2015.txt
+      1	cod
+      1	flying fish
+      2	marlin
+      3	shark
+      1	tuna
+      1	turtle
+data/matt_2014.txt
+      2	turtle
+data/matt_2015.txt
+      2	turtle
